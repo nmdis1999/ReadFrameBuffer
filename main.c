87e3f64ec940df9d97c140b11890302902151e20
Supragya Raj <supragyaraj@gmail.com>
2018-06-02 00:52:43 +0530

    formatting

--- main.c
+++ main.c
@@ -15,28 +15,28 @@ static uint32_t map_addr = 0x00000000;
 static char *dev_mem = "/dev/mem";
 
 int main(int argc, char **argv) {
-  int fd;
-  void *buf;
-  if ((fd = open(dev_mem, O_RDWR | O_SYNC)) == -1) {
-    printf("can't open /dev/mem .\n");
-    exit(EXIT_FAILURE);
-  }
+    int fd;
+    void *buf;
+    if ((fd = open(dev_mem, O_RDWR | O_SYNC)) == -1) {
+        printf("can't open /dev/mem .\n");
+        exit(EXIT_FAILURE);
+    }
 
-  buf = mmap((void *)map_addr, map_size, PROT_READ | PROT_WRITE, MAP_SHARED, fd,
-             map_base);
-  if (buf == (void *)-1) {
-    printf("Can't be mapped. \n");
-    exit(EXIT_FAILURE);
-  } else
-    map_addr = (long unsigned)buf;
+    buf = mmap((void *) map_addr, map_size, PROT_READ | PROT_WRITE, MAP_SHARED, fd,
+               map_base);
+    if (buf == (void *) -1) {
+        printf("Can't be mapped. \n");
+        exit(EXIT_FAILURE);
+    } else
+        map_addr = (long unsigned) buf;
 
-  fwrite(buf, 1, map_size, stdout);
+    fwrite(buf, 1, map_size, stdout);
 
-  int unmap_file = munmap(buf, map_size);
+    int unmap_file = munmap(buf, map_size);
 
-  close(fd);
+    close(fd);
 
-  return 0;
+    return 0;
 }
 
 
